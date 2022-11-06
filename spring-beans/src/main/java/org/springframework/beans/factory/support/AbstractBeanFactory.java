e03e1013de46770bd18d8bf0521fe485f1b33ea7
gxg <498868779@qq.com>
2022-11-06 14:49:14 +0800

    spring容器研究20221030

--- spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -239,6 +239,7 @@ public abstract class AbstractBeanFactory extends FactoryBeanRegistrySupport imp
 	protected <T> T doGetBean(final String name, @Nullable final Class<T> requiredType,
 			@Nullable final Object[] args, boolean typeCheckOnly) throws BeansException {
 
+		// 转换bean名称
 		final String beanName = transformedBeanName(name);
 		Object bean;
 
